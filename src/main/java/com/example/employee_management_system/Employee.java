7eb522ae5f3feb6847faff5b80584d09c67a9722
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2025-04-14 16:57:51 +0000

    Employee and CRUD functionalities done

---
+++ src/main/java/com/example/employee_management_system/Employee.java
@@ -0,0 +1,87 @@
+package com.example.employee_management_system;
+
+
+public class Employee<T> implements Comparable<Employee<T>> {
+    private T employeeID;
+    private String name;
+    private String department;
+    private double salary;
+    private double performanceRating;
+    private int yearsOfExperience;
+    private boolean isActive;
+
+    public Employee(T employeeID, String name, String department, double salary, double performanceRating, int yearsOfExperience, boolean isActive){
+        this.employeeID = employeeID;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.performanceRating = performanceRating;
+        this.yearsOfExperience = yearsOfExperience;
+        this.isActive = isActive;
+    }
+
+
+//    Getters
+    public T getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public double getPerformanceRating() {
+        return performanceRating;
+    }
+
+    public int getYearsOfExperience(){
+        return yearsOfExperience;
+    }
+
+    public boolean getIsActive(){
+        return isActive;
+    }
+
+//    Setters
+    public void setEmployeeID(T employeeID){
+        this.employeeID = employeeID;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience){
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public void setPerformanceRating(double performanceRating){
+        this.performanceRating = performanceRating;
+    }
+
+    public void setActive(boolean isActive){
+        this.isActive = isActive;
+    }
+
+//    arrange Employees in descending order
+    @Override
+    public int compareTo(Employee<T> other){
+        return Integer.compare(other.yearsOfExperience, this.yearsOfExperience);
+    }
+}
---
+++ src/main/java/com/example/employee_management_system/Search.java
@@ -0,0 +1,9 @@
+package com.example.employee_management_system;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+
+public class Search {
+
+}
